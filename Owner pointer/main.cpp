6c696afbdd4f66330ab75b039eb5a4eab6a862a2
14bscsmzafar <14bscsmzafar@seecs.edu.pk>
2016-12-06 23:34:33 +0500

    Correct the git upload path

---
+++ Owner pointer/main.cpp
@@ -0,0 +1,56 @@
+/* 
+ * File:   main.cpp
+ *
+ * Created on February 16, 2016, 9:51 PM
+ */
+
+#include <iostream>
+#include<cstring>
+#include "StringBuffer.h"
+#include "String.h"
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) 
+{
+    //create a smart string
+	//String ss;
+    //create a smart string with const char*
+
+    char* hello = "thisisalongstring";
+ 
+    
+    
+    String ss2(hello,17);
+       
+	cout<<"ss2 length = " << ss2.length()<<std::endl;
+    cout<<hello<<endl;
+    //multiple references
+    String ss(&ss2);
+    
+    
+    
+    //output statements
+    std::cout<<"ss length = "<<ss.length()<<std::endl;
+    std::cout<<"new ss charAt 0 = "<<ss.charAt(0)<<std::endl;
+    
+    
+    
+    
+    //append a character
+    ss.append('w');
+        
+    //cout<<hello<<endl;
+    //hello = "bye";
+    
+    std::cout<<"new ss length = "<<ss.length()<<std::endl;
+    std::cout<<"new ss2 length = "<<ss2.length()<<std::endl;
+    
+    
+    //char* temp=ss.getBuff();
+	system("pause");
+    return 0;
+}
+
